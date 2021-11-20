4730ade2a92f6dfe2c1f0e03799363cb93bbb456
907VigiL <94725239+907VigiL@users.noreply.github.com>
2021-11-19 15:44:42 -0900

    Add files via upload

---
+++ PhysicsTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsTest : MonoBehaviour
+{
+
+    public Vector3 P;
+    public Vector3 V;
+    public Vector3 A;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float deltaT = Time.deltaTime;
+        V += A * deltaT;
+        P += V * deltaT;
+        transform.position = P;
+    }
+}
