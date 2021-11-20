9e0f40284f5838aca8b34ec78696aa5db065d88f
907VigiL <94725239+907VigiL@users.noreply.github.com>
2021-11-19 15:45:07 -0900

    Add files via upload

---
+++ FireLaser.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireLaser : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        if(!Physics.Raycast(transform.position, transform.right, out hit)) return;
+
+        Collider c = hit.collider;
+        GameObject obj = c.gameObject;
+        MomentumSpinner mom = obj.GetComponent<MomentumSpinner>();
+        if(mom)
+        {
+            Vector3 direction = transform.right;
+            mom.ApplyTorque(hit.point, direction, Time.deltaTime);
+
+            Vector3 flyDir = hit.normal;
+            flyDir += Random.insideUnitSphere*0.5f;
+            EmitParticle.Now(hit.point, flyDir*10.0f, new Color(1.0f, 1.0f, 1.0f, 1.0f));
+        }
+    }
+}
